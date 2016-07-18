2ca1b08bca78f37afb7d825588792ee57b4acb49
Tim Pender <mrtimpender@gmail.com>
2016-07-18 16:02:29 -0600

    added about content

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* HOME GET */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Let's Capture Something Special" });
+});
+
+/* ABOUT GET */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
