b181e66d537230b39ca0b1f86b32ec9e6e116259
Tim Pender <mrtimpender@gmail.com>
2016-07-26 00:55:48 -0600

    added nav, site routes.  Desktop base nav styling

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,17 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
 
+/* Contact GET */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {title: 'CONTACT'});
+})
+/* Portfolio GET */
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', {title: 'PORTFOLIO'});
+});
+/* SERVICES GET */
+router.get('/services', function(req, res, next) {
+    res.render('services', {title: 'SERVICES'});
+  });
+
 module.exports = router;
