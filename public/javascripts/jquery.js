d15ad4827208d5280c269031046400659e25b900
Tim Pender <mrtimpender@gmail.com>
2016-08-01 09:22:20 -0600

    pushing about changes

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -5,4 +5,7 @@ $(document).ready(function(){
       selectMonths: true, // Creates a dropdown to control month
       selectYears: 5 // Creates a dropdown of 15 years to control year
     });
+
+  $('.carousel').carousel();
+
 }) // JQuery Document.Ready Closure
