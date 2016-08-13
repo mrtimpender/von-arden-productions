df35a903b5dcb8ddc823510a979420036862f05b
Tim Pender <mrtimpender@gmail.com>
2016-08-13 15:37:30 -0600

    pre-depoyment

--- routes/index.js
+++ routes/index.js
@@ -23,5 +23,9 @@ router.get('/portfolio', function(req, res, next) {
 router.get('/services', function(req, res, next) {
     res.render('services', {title: 'Services'});
   });
+  /* CONTACT GET */
+  router.post('/contact', function(req, res, next) {
+      res.send(req.body);
+    });
 
 module.exports = router;
