3d1358f76fbcfc0f7c2d1d0f2cd3a694394277fc
Tim Pender <mrtimpender@gmail.com>
2016-08-14 20:30:28 -0600

    mobile nav added

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  $(".button-collapse").sideNav();
 
   $('.datepicker').pickadate({
       dateFormat: 'mm/dd/yy',
--- routes/index.js
+++ routes/index.js
@@ -23,9 +23,14 @@ router.get('/portfolio', function(req, res, next) {
 router.get('/services', function(req, res, next) {
     res.render('services', {title: 'Services'});
   });
-  /* CONTACT GET */
-  router.post('/contact', function(req, res, next) {
-      res.send(req.body);
-    });
+/* Pricing Get */
+router.get('/pricing', function(req, res, next){
+  res.render('pricing', {title: 'Pricing'})
+});
+/* CONTACT Post */
+router.post('/contact', function(req, res, next) {
+    res.send(req.body);
+});
+
 
 module.exports = router;
