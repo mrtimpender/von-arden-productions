c131b75cf3406c5db62dc3dc4e0ac7fc7dea7062
Tim Pender <mrtimpender@gmail.com>
2016-08-28 16:17:12 -0600

    setup heroku db

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/vonarden_development'
+    connection: process.env.DATABASE_URL || 'postgres://localhost/vonarden_development'
   },
 
   production: {
